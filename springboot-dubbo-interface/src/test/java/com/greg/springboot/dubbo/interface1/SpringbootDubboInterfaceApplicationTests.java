bcc46a7499e695f92ce3dc55bcdde6cc59e08918
greg <lecoboy@163.com>
2021-12-28 00:37:52 +0800

    commit

---
+++ springboot-dubbo-interface/src/test/java/com/greg/springboot/dubbo/interface1/SpringbootDubboInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greg.springboot.dubbo.interface1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDubboInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
